0f89b84dfbfb8d78a5e0788b5f3b6588092a8ef0
Spencer Owen <spencerowen@Spencers-MacBook-Pro.local>
2014-01-11 00:48:28 -0700

    Adds users to yaml

--- test/ruby-loadfile.rb
+++ test/ruby-loadfile.rb
@@ -3,29 +3,69 @@ class Roomate
 end
 
 require 'yaml'
-aListOfRoomates = YAML.load_file('roomates.yaml')
-#puts aListOfRoomates.inspect
+$aListOfRoomates = []
+#loadRoomates
 
-puts aListOfRoomates.first.name
-puts aListOfRoomates[2].name
-
-aListOfRoomates.each do | num|
-	puts num.name
-end
 
 # data = 
 # File.oopen('roomates.yaml', 'w') { 
 # 	|f| YAML.dump(data, f)
 # }
 
-aNewRoomate = Roomate.new
-
-aNewRoomate.name = "bob"
-aNewRoomate.computers = "33:33:33:33:33"
-aNewRoomate.paid = 2014-01-02
 
 
-aListOfRoomates.push(aNewRoomate)
-File.open('roomates.yaml', 'w') {
-	|f| f.write(YAML::dump(aListOfRoomates))
-}
+def addMacAddress(aRoomate, aMacAddress)
+	return nil
+end
+
+def addUser(aName)
+	aNewRoomate = Roomate.new
+	aNewRoomate.name = aName
+	aNewRoomate.computers = ""
+	aNewRoomate.paid = Date.parse('2014-01-02')
+	$aListOfRoomates.push(aNewRoomate)
+	saveRoomates
+	return nil
+end
+
+def removeUser(aRoomate)
+	return nil
+end
+
+def changePayment(aRoomate)
+	return nil
+end
+
+def validatePayment(aRoomate)
+	return hasPaid
+end
+
+def blockUsers(aListOfRoomates)
+	return nil
+end
+
+def loadRoomates()
+	$aListOfRoomates = YAML.load_file('roomates.yaml')
+	return nil
+end
+
+def saveRoomates()
+	File.open('roomates.yaml', 'w') {
+		|f| f.write(YAML::dump($aListOfRoomates))
+	}
+end
+
+loadRoomates
+
+
+puts $aListOfRoomates[0].name
+
+addUser("joe")
+# puts aListOfRoomates[2].name
+
+# aListOfRoomates.each do | num|
+# 	puts num.name
+# end
+
+
+
