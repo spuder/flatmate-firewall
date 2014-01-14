ff6c4c1faacaa3a3e11cffebeea60a4cede1df6f
Spencer Owen <owenspencer@gmail.com>
2014-01-13 22:17:07 -0700

    Adds tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,33 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_index_path
-      response.status.should be(200)
-    end
+
+
+  # describe "GET /static_pages" do
+  #   it "works! (now write some real specs)" do
+  #     # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+  #     get static_pages_index_path
+  #     response.status.should be(200)
+  #   end
+  # end
+
+  describe "Home Page" do
+
+    it "verifies the home page has the word home page" do
+  	  visit '/static_pages/home'
+  	  expect(page).to have_content('Home')
+  	end
+
   end
+
+  describe "Admin Page" do
+
+  	it "verifies the admin page has the word admin page" do
+	  visit '/static_pages/admin'
+  	  expect(page).to have_content('Admin')
+  	end
+  	
+  end
+
+
 end
