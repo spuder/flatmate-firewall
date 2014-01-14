54cd83f63e8db04103ac92ec4d1df01340088285
Spencer Owen <owenspencer@gmail.com>
2014-01-13 22:11:08 -0700

    Adds validation & static_pages tests

--- app/models/computer.rb
+++ app/models/computer.rb
@@ -1,3 +1,5 @@
 class Computer < ActiveRecord::Base
 	belongs_to :user
+	validates :mac_address, :length => { :maximum => 17 }
+	validates_numericality_of :user_id, :only_integer => true, :greater_than_or_equal_to => 0 
 end
